4b8c9d7c257373adc190521607f7a32970cf562b
Treybrowder <tbrowder7@gmail.com>
2024-08-16 17:07:31 -0400

    First commit - initial set up

---
+++ DessertsApp/App/DessertsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsAppApp.swift
+//  DessertsApp
+//
+//  Created by Trey Browder on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ DessertsApp/Core/Home/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertsApp
+//
+//  Created by Trey Browder on 8/16/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
