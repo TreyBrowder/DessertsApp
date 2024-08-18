071c859ae869d0e9c008bafa8fdb36a0d03a172a
Treybrowder <tbrowder7@gmail.com>
2024-08-18 10:40:50 -0400

    Removing file that shouldn't be tracked

---
+++ DessertsApp/Core/AllDeserts/Model/DessertData.swift
@@ -0,0 +1,8 @@
+//
+//  DessertData.swift
+//  DessertsApp
+//
+//  Created by Trey Browder on 8/18/24.
+//
+
+import Foundation
---
+++ DessertsApp/Core/AllDeserts/ViewModel/DessertsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DessertsViewModel.swift
+//  DessertsApp
+//
+//  Created by Trey Browder on 8/18/24.
+//
+
+import Foundation
