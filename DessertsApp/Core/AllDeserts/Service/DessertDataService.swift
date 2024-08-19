be8bf8a385e84a01ce55b827c611637f87a1f7c0
Treybrowder <tbrowder7@gmail.com>
2024-08-19 08:25:14 -0400

    added missing changes to stage

--- DessertsApp/Core/AllDeserts/Service/DessertDataService.swift
+++ DessertsApp/Core/AllDeserts/Service/DessertDataService.swift
@@ -7,51 +7,59 @@
 
 import Foundation
 
-
-protocol DataDownloaderProtocol {
-    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T
-}
-
-extension DataDownloaderProtocol {
-    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T {
-        guard let url = URL(string: endpoint) else {
-            throw DessertAPIError.requestFailed(description: "Invalid URL")
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw DessertAPIError.requestFailed(description: "Request Failed")
-        }
-        
-        guard httpResponse.statusCode == 200 else {
-            throw DessertAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
-        }
-        
-        do {
-            return try JSONDecoder().decode(type, from: data)
-           
-        } catch {
-            print("DEBUG - Service ERROR: \(error)")
-            throw error as? DessertAPIError ?? .unknownError(error: error)
-        }
-    }
-}
-
 class DessertDataService: DataDownloaderProtocol {
     
-    private let urlString = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+//MARK: - URL Retated variables/functions
+    private var baseUrlComponents: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "themealdb.com"
+        components.path = "/api/json/v1/1/"
+        
+        return components
+    }
+    
+    
+    private var allDessertsUrlString: String? {
+        var components = baseUrlComponents
+        components.path += "filter.php"
+        
+        components.queryItems = [
+            .init(name: "c", value: "Dessert")
+        ]
+        
+        return components.url?.absoluteString
+    }
+    
+    private func dessertDetailsUrlString(id: String) -> String? {
+        var components = baseUrlComponents
+        components.path += "/lookup.php"
+        
+        components.queryItems = [
+            .init(name: "i", value: "\(id)")
+        ]
+        
+        return components.url?.absoluteString
+    }
+    
+//MARK: - Fetch Data Methods
     
     ///Asychronous function to retreive dessert data from API
     func fetchDesserts() async throws -> Dessert {
-        return try await fetchData(as: Dessert.self, endpoint: urlString)
+        guard let endpoint = allDessertsUrlString else {
+            throw DessertAPIError.requestFailed(description: "Invalid URL")
+        }
         
+        return try await fetchData(as: Dessert.self, endpoint: endpoint)
     }
     
     ///Asychronous function to retreive dessert details data from API
     func fetchDessertDetails(id: String) async throws -> DessertDetails {
-        let detailsUrlString = "https://themealdb.com/api/json/v1/1/lookup.php?i=\(id)"
-        return try await fetchData(as: DessertDetails.self, endpoint: detailsUrlString)
+        guard let endpoint = dessertDetailsUrlString(id: id) else {
+            throw DessertAPIError.requestFailed(description: "Invalid URL")
+        }
+        
+        return try await fetchData(as: DessertDetails.self, endpoint: endpoint)
     }
     
 }
--- DessertsApp/Shared/API/DataDownloader.swift
+++ DessertsApp/Shared/API/DataDownloader.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol DataDownloaderProtocol {
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T
+}
+
+extension DataDownloaderProtocol {
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T {
+        guard let url = URL(string: endpoint) else {
+            throw DessertAPIError.requestFailed(description: "Invalid URL")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw DessertAPIError.requestFailed(description: "Request Failed")
+        }
+        
+        guard httpResponse.statusCode == 200 else {
+            throw DessertAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(type, from: data)
+           
+        } catch {
+            print("DEBUG - Service ERROR: \(error)")
+            throw error as? DessertAPIError ?? .unknownError(error: error)
+        }
+    }
+}
