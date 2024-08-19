ef0beaaac952891dc07c3b055f7c3b4fba97043c
Treybrowder <tbrowder7@gmail.com>
2024-08-19 08:01:49 -0400

    refactor data service class

--- DessertsApp/Core/AllDeserts/Service/DessertDataService.swift
+++ DessertsApp/Core/AllDeserts/Service/DessertDataService.swift
@@ -7,60 +7,51 @@
 
 import Foundation
 
-class DessertDataService {
+
+protocol DataDownloaderProtocol {
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T
+}
+
+extension DataDownloaderProtocol {
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T {
+        guard let url = URL(string: endpoint) else {
+            throw DessertAPIError.requestFailed(description: "Invalid URL")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw DessertAPIError.requestFailed(description: "Request Failed")
+        }
+        
+        guard httpResponse.statusCode == 200 else {
+            throw DessertAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(type, from: data)
+           
+        } catch {
+            print("DEBUG - Service ERROR: \(error)")
+            throw error as? DessertAPIError ?? .unknownError(error: error)
+        }
+    }
+}
+
+class DessertDataService: DataDownloaderProtocol {
     
     private let urlString = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
     
     ///Asychronous function to retreive dessert data from API
     func fetchDesserts() async throws -> Dessert {
-        guard let url = URL(string: urlString) else {
-            throw DessertAPIError.requestFailed(description: "Invalid URL")
-        }
+        return try await fetchData(as: Dessert.self, endpoint: urlString)
         
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw DessertAPIError.requestFailed(description: "Request Failed")
-        }
-        
-        guard httpResponse.statusCode == 200 else {
-            throw DessertAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
-        }
-        
-        do {
-            let desserts = try JSONDecoder().decode(Dessert.self, from: data)
-            return desserts
-        } catch {
-            print("DEBUG - Service ERROR: \(error)")
-            throw error as? DessertAPIError ?? .unknownError(error: error)
-        }
     }
     
     ///Asychronous function to retreive dessert details data from API
     func fetchDessertDetails(id: String) async throws -> DessertDetails {
-        
         let detailsUrlString = "https://themealdb.com/api/json/v1/1/lookup.php?i=\(id)"
-        
-        guard let url = URL(string: detailsUrlString) else {
-            throw DessertAPIError.requestFailed(description: "Invalid URL")
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw DessertAPIError.requestFailed(description: "Request Failed")
-        }
-        
-        guard httpResponse.statusCode == 200 else {
-            throw DessertAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
-        }
-        
-        do {
-            let details = try JSONDecoder().decode(DessertDetails.self, from: data)
-            return details
-        } catch {
-            print("DEBUG - Service ERROR: \(error)")
-            throw error as? DessertAPIError ?? .unknownError(error: error)
-        }
+        return try await fetchData(as: DessertDetails.self, endpoint: detailsUrlString)
     }
+    
 }
